a94d4961d5b1cf5506560f07aa8c1f5364abf0be
Josema <josema.saborido@gmail.com>
2021-12-18 19:09:42 +0100

    Initial commit

---
+++ src/main/java/es/josemasaborido/FirstCommit/FirstCommitApplication.java
@@ -0,0 +1,13 @@
+package es.josemasaborido.FirstCommit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCommitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/josemasaborido/FirstCommit/FirstCommitApplicationTests.java
@@ -0,0 +1,13 @@
+package es.josemasaborido.FirstCommit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCommitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
