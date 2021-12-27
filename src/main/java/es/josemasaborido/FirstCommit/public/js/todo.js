a8cbd0e019a3a4ed8877d64e7d0cf2751e6f96bc
Josema <josema.saborido@gmail.com>
2021-12-27 23:01:35 +0100

    subida modificaciones js y html

--- src/main/java/es/josemasaborido/FirstCommit/public/js/todo.js
+++ src/main/java/es/josemasaborido/FirstCommit/public/js/todo.js
@@ -3,7 +3,7 @@ console.log("LLEGO EL MOMENTO DEL CAOS");
 const listadoAlumnosJSON = [
     {
         "nombre" : "josema",
-        "ciudad" : "El Puerto de Santa María",
+        "ciudad" : "Cádiz",
         "pais" : "España",
         "telefono" : "666111555",
         "email" : "j.saborido@josemasaborido.es",
@@ -101,7 +101,9 @@ const listadoAlumnosJSON = [
             }
         ]
     }];
-
+/**
+ * Clase que define un alumno
+ */
 class Alumno{
 
     //ATRIBUTOS
@@ -114,7 +116,15 @@ class Alumno{
     #etiquetas;
 
     //CONSTRUCTORES
-
+    /**
+     * Constructor con parametros
+     * @param {*} nombreCompleto nombre del alumno
+     * @param {*} ciudad ciudad en la que vive el alumno
+     * @param {*} pais pais en el que vive el alumno
+     * @param {*} telefono telefono del alumno
+     * @param {*} email email del alumno
+     * @param {*} etiquetas certificaciones conseguidas por el alumno en array
+     */
     constructor(nombreCompleto, ciudad, pais, telefono, email, etiquetas){
         this.#nombreCompleto = nombreCompleto;
         this.#ciudad = ciudad;
@@ -177,7 +187,9 @@ class Alumno{
 
 
 }
-
+/**
+ * Clase que define la gestion del los datos obtenidos a partir de un JSON
+ */
 class JsonService{
 
     //ATRIBUTOS
@@ -185,10 +197,11 @@ class JsonService{
     #listadoAlumnosJSON;
 
     //CONSTRUCTORES
-    /*
-        Constructor con parametros
-        @lisdoAlumnosJson es el listado de alumnos en formato JSON
-
+  
+    /**
+     * Constructor con parametros
+     * 
+     * @param {*} listadoAlumnosJSON  es el listado de alumnos en formato JSON
      */
     constructor(listadoAlumnosJSON){
         this.#listadoAlumnosJSON = listadoAlumnosJSON;
@@ -196,11 +209,11 @@ class JsonService{
 
     //METODOS
 
-    /*
-        Metodo que obtiene un array de objetos Alumno y
-        los devulve para su tratamiento
-        Si todo es correcto devulve un array con los objetos
-        Si hay algun error devulve null
+    /** 
+     *  Metodo que obtiene un array de objetos Alumno y
+     *  los devulve para su tratamiento
+     * 
+     * @returns Si todo es correcto devuelve un array con los objetos. Si hay algun error devuelve null
      */
     obtenerTodos(){
         //Definimos un array donde iran los objetos alumnos
@@ -237,9 +250,11 @@ class JsonService{
 
 }
 
-const jsonService = new JsonService(listadoAlumnosJSON);
-console.log(jsonService.obtenerTodos()[1]);
 
+
+/**
+ * Clase que gestiona la generación de la tabla de alumnos
+ */
 class ViewListadoAlumnos{
     
     //ATRIBUTOS
@@ -248,11 +263,11 @@ class ViewListadoAlumnos{
     #jsonService;
 
     //CONSTRUCTORES
-    /*
-        Constructor sin parametros
-        Instanciamos el services, el repository 
-        y el tipo de almacenamiento
-    */
+    /**
+     * Constructor con parametros
+     * @param {*} listadoAlumnosJSON  el json con los datos de los alumnos
+     * @param {*} jsonService objeto del servicio jsonService
+     */
     constructor(listadoAlumnosJSON, jsonService){
         console.log("Estamos en el constrcutor de View");
         this.#tipoAlmacenamiento = listadoAlumnosJSON;
@@ -262,10 +277,10 @@ class ViewListadoAlumnos{
     }
 
     //METODOS
-    /*
-        Metodo encargado de componer la tabla con los alumnos
-        devulve un string para procesarlo posteriormente como html
-    */
+    /**
+     * Metodo encargado de componer la tabla con los alumnos
+     *   devulve un string para procesarlo posteriormente como html
+     */
     componerTabla(){
         //Obtenemos el listado de Alumnos
         let listadoAlumnos = this.obtenerListado();
@@ -292,10 +307,11 @@ class ViewListadoAlumnos{
         document.getElementById("datos").innerHTML = tabla;
     }
 
-    /*
-        Metodo que obtiene el Listado de Alumnos almacenados
+    /**
+     * Metodo que obtiene el Listado de Alumnos almacenados
         devulve un array de alumnos
-    */
+     * @returns listado de alumnos
+     */
     obtenerListado(){
         //metemos en una variable el array[Alumnos]
         let listadoAlumnos = this.#jsonService.obtenerTodos();
@@ -321,7 +337,167 @@ class ViewListadoAlumnos{
     }
 
 }
+
+/**
+ * Clase que define el funcionamiento del filtro de la tabla de alumnos
+ */
+class FiltroOrdenarTabla{
+    //ATRIBUTOS
+    #filtroElementID;
+    #tipoAlmacenamiento;
+    #contador = 0; //Si es impar ordena ascendente y si es par
+
+    //CONSTRUCTORES
+    /**
+     * 
+     * @param {*} filtroElementID referencia del elemento ID del documento HTML
+     * @param {*} tipoAlmacenamiento fuente de datos
+     */
+    constructor(filtroElementID, tipoAlmacenamiento){
+        this.#filtroElementID = filtroElementID;
+        this.#tipoAlmacenamiento = tipoAlmacenamiento;
+    }
+
+    //METODOS
+    /**
+     * Metodo que ordena de forma ascendente o descendente el listado de alumnos por el tipo de filtro selccionado
+     */
+    ordenar(){
+        console.log("Estamos dentro de ordenar");
+        //asignamos el elemento pasado por parametro a una constante
+        const elemento = document.getElementById(this.#filtroElementID);
+        elemento.onclick = this.aumentarContador();
+        console.log(this.getContador) //Si se hace click en el elemento aumenta el contador
+        if(this.#contador > 0){//si el elemento se ha pulsado al menos una vez
+            if(this.#contador%2 == 0){
+                console.log("Estamos eligiendo orden descendente"); //si el valor del contador es par ordena de forma descendente
+                this.ordenDescendente(this.#filtroElementID);
+
+            }else {//Si el valor del contador es impar ordena de forma ascendente
+                console.log("Estamos eligiendo orden ascendente");
+                this.ordenAscendente(this.#filtroElementID);
+            }
+        }
+    }
+
+    ordenAscendente(filtroElementID){
+        console.log("Estamos dentro de orden Ascendente");
+        console.log(filtroElementID);
+        //Creamos un array
+        let lista = [];
+        //Asignamos el listado de alumnos
+        switch(filtroElementID){
+            case "filtroNombre":
+                console.log("Estamos dentro de switch ASCENDENTE filtroNombre");
+                lista = this.#tipoAlmacenamiento.sort(this.sortArrayNombre);
+                console.log(lista);
+                break;
+            case "filtroCiudad":
+                lista.sort((a, b) => (a.ciudad > b.ciudad)? 1 : -1);
+                break;
+            case "filtroPais":
+                lista.sort((a, b) => (a.pais > b.pais)? 1 : -1);
+                break;
+            case "filtroTelefono":
+                lista.sort((a, b) => (a.telefono > b.telefono)? 1 : -1);
+                break;
+            case "filtroEmail":
+                lista.sort((a, b) => (a.email > b.email)? 1 : -1);
+                break;
+            case "filtroEtiquetas":
+                lista.sort((a, b) => (a.etiquetas > b.etiquetas)? 1 : -1);
+                break;
+        }
+
+    }
+
+    ordenDescendente(filtroElementID){
+        console.log("Estamos dentro de orden Descendente");
+        //Creamos un array
+        let lista = [];
+        //Asignamos el listado de alumnos
+        lista = this.#tipoAlmacenamiento;
+        switch(filtroElementID){
+            case "filtroNombre":
+                console.log("Estamos dentro de switch DESCENDENTE filtroNombre");
+                lista = this.#tipoAlmacenamiento.reverse(this.sortArrayNombre);
+                console.log(lista);
+                break;
+            case "filtroCiudad":
+                lista.sort((a, b) => (a.ciudad > b.ciudad)? -1 : 1);
+                break;
+            case "filtroPais":
+                lista.sort((a, b) => (a.pais > b.pais)? -1 : 1);
+                break;
+            case "filtroTelefono":
+                lista.sort((a, b) => (a.telefono > b.telefono)? -1 : 1);
+                break;
+            case "filtroEmail":
+                lista.sort((a, b) => (a.email > b.email)? -1 : 1);
+                break;
+            case "filtroEtiquetas":
+                lista.sort((a, b) => (a.etiquetas > b.etiquetas)? -1 : 1);
+                break;
+        }
+
+    }
+
+    aumentarContador(){
+        this.setContador = this.#contador + 1;
+    }
+
+    sortArrayNombre(a, b){
+        if(a.nombre > b.nombre){return 1}
+        if(a.nombre < b.nombre){return -1}
+        return 0;
+    }
+
+    //GETTER Y SETTER
+    get getFiltroElementID(){
+        return this.#filtroElementID;
+    }
+
+    set setFiltroElementID(filtroElementID){
+        this.#filtroElementID = filtroElementID;
+    }
+
+    get getTipoAlmacenamiento(){
+        return this.#tipoAlmacenamiento;
+    }
+
+    set setTipoAlmacenamiento(tipoAlmacenamiento){
+        this.#tipoAlmacenamiento = tipoAlmacenamiento;
+    }
+
+    get getContador(){
+        return this.#contador;
+    }
+
+    set setContador(numero){
+        this.#contador = numero;
+    }
+}
+
+/*EJECUCION DEL PROGRAMA*/
+//Creamos los filtros
+const filtroNombre = new FiltroOrdenarTabla("filtroNombre", listadoAlumnosJSON);
+const jsonService = new JsonService(listadoAlumnosJSON);
 const view = new ViewListadoAlumnos(listadoAlumnosJSON, jsonService);
+
+function filtrarNombre(){
+    console.log("Se ha activado filtrarNombre");
+    filtroNombre.ordenar();
+}
+const filtroCiudad = new FiltroOrdenarTabla("filtroCiudad", listadoAlumnosJSON);
+function filtrarCiudad(){
+    filtroCiudad.ordenar();
+}
+const filtroPais = new FiltroOrdenarTabla("filtroPais", listadoAlumnosJSON);
+const filtroTelefono = new FiltroOrdenarTabla("filtroTelefono", listadoAlumnosJSON);
+const filtroEmail = new FiltroOrdenarTabla("filtroEmail", listadoAlumnosJSON);
+const filtroEtiquetas = new FiltroOrdenarTabla("filtroEtiquetas", listadoAlumnosJSON);
+
+
 console.log(view.obtenerListado());
 
 window.onload= view.componerTabla();
